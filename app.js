41732483823470745dc339d641c8a0997cdefbdd
Joe Williams <jw1540@york.ac.uk>
2016-04-16 12:10:36 +0100

    use geolocation to determine user location rather than search

--- app.js
+++ app.js
@@ -6,36 +6,37 @@ new Vue({
     return {
       city: '',
       format: true,
-      img: {}
+      lat: '',
+      lon: ''
     }
   },
-
+  ready: function(){
+    this.getData();
+  },
   methods: 
   {
-    getData: function(){
-      var self = this; 
-      $.getJSON('http://api.openweathermap.org/data/2.5/weather?q='+self.city+'&units=metric&appid=55e71b08f9edeb8c17b7eb7f381bf15d', function(data){
+    getData: function()
+    {
+      $('.uil-sunny.css').show();
+      var self = this;
+      function success(pos){
+        var latitude  = pos.coords.latitude;
+        var longitude = pos.coords.longitude;
+      $.getJSON('http://api.openweathermap.org/data/2.5/find?lat='+latitude+'&lon='+longitude+'&cnt=10&appid=55e71b08f9edeb8c17b7eb7f381bf15d&units=metric', function(data){
         self.$set('data', data);
       })
-      $.ajax({
-        type: "GET",
-        
-        url: "https://api.webcams.travel/rest?method=wct.webcams.list_by_tag&devid=0a4c346c97281912908108bbc20036e0&tag="+self.city+"&format=json",
-        data: null
-      })
-      .success(function (data, textStatus, jqXHR) { self.$set('img', data) }.bind(this))
-      .fail(function (data, err) { console.log(err)});
+      }
+      navigator.geolocation.getCurrentPosition(success);
+      $('.uil-sunny.css').hide();
     },
-    change: function(){
+    change: function()
+    {
       var self = this;
       if(self.format){
         return self.format = false;
       }else {
         return self.format = true;
       }
-    },
-    background: function(){
-      var self = this;
     }
   }
 })
